926ac64b8bc9c5a8f0f641aaceb57bd203c94008
Zhaohan Hou (侯兆晗) <houzhaohan@yeah.net>
2026-09-13 08:25:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def seed_default_admin():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3031, debug=True)
